055cdfb6fbec96c14d44a5b053eaa3e0a35a43fe
jaswinder0612 <jaswinder.jk.07@gmail.com>
2021-09-25 16:10:38 +0530

    files add

---
+++ src/fireBase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDxZYvotHZvwiwpODHpPLdBcjzn5O-VfTI",
+    authDomain: "tictok-8b430.firebaseapp.com",
+    projectId: "tictok-8b430",
+    storageBucket: "tictok-8b430.appspot.com",
+    messagingSenderId: "379942273516",
+    appId: "1:379942273516:web:14413886388475777580c9"
+  };
+
+const app = initializeApp(firebaseConfig);
